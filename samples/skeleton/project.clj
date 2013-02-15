08e1c8c5565fd6efeb0dd4ae2e8b62eeb7584188
David Bergman <davber@gmail.com>
2013-02-14 22:18:25 -0500

    Changed the path of the cljsutils directory in skeleton project

--- samples/skeleton/project.clj
+++ samples/skeleton/project.clj
@@ -2,10 +2,10 @@
   :description "Skeleton sample for the Clotitanium library"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :plugins [[lein-cljsbuild "0.3.0"]]
-  :source-paths ["../../Resources/modules/cljsutils/cljs-macros" "../../cljs-macros"]
+  :source-paths ["../../modules/cljsutils/cljs-macros" "../../cljs-macros"]
   :cljsbuild {
     :builds [
-      {:source-paths ["../../Resources/modules/cljsutils/cljs"  "../../cljs" "cljs" ]
+      {:source-paths ["../../modules/cljsutils/cljs"  "../../cljs" "cljs" ]
        :compiler {
        	:output-to "Resources/public/cljs/main.js"
        	:optimizations :whitespace
