40b95c0b5b410d82b82d672f07d5a363f3b40f9f
David Bergman <davber@gmail.com>
2013-02-14 21:58:48 -0500

    Deleted generated JS files in skeleton project

--- skeleton/Resources/public/cljs/styles.js
+++
@@ -1,9 +0,0 @@
-goog.provide('styles');
-goog.require('cljs.core');
-goog.require('ti');
-styles._STAR_default_bg_color_STAR_ = "white";
-styles._STAR_default_fg_color_STAR_ = "black";
-styles._STAR_default_bg_image = null;
-styles._STAR_default_bar_color_STAR_ = "blue";
-styles._STAR_default_bar_image_STAR_ = null;
-styles._STAR_default_config_STAR_ = cljs.core.ObjMap.fromObject(["stdWin"],{"stdWin":cljs.core.ObjMap.fromObject(["\uFDD0:backgroundColor","\uFDD0:backgroundImage","\uFDD0:barColor","\uFDD0:barImage","\uFDD0:navBarHidden"],{"\uFDD0:backgroundColor":"transparent","\uFDD0:backgroundImage":styles._STAR_default_bg_image_STAR_,"\uFDD0:barColor":styles._STAR_default_bar_color_STAR_,"\uFDD0:barImage":styles._STAR_default_bar_image_STAR_,"\uFDD0:navBarHidden":false})});
